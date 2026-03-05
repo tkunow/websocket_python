5b731638483b0b2523d5f03ac49fbf174e6b454d
tkunow <t.kunow99@googlemail.com>
2026-03-05 13:52:34 +0100

    proper endpoint disconnect

--- example/example.py
+++ example/example.py
@@ -2,13 +2,11 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from websocket.asyncwebsocket import Websocket
+from websocket.asyncwebsocket import Websocket, WebsocketDisconnect
 import asyncio
 import logging
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-
+async def main():
     ws = Websocket(["127.0.0.1", 80])
     @ws
     async def ws_loop(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
@@ -22,7 +20,17 @@ if __name__ == "__main__":
 
                     await ws_loop.send_frame(writer, msg)
 
-        finally:
-            print("something went wrong")
-    
-    asyncio.run(ws.start())
+        except WebsocketDisconnect:
+            await ws_loop.disconnect(writer)
+    try:
+        await ws.start()
+    finally:
+        await ws.close()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
